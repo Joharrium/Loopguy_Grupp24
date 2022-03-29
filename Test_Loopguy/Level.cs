916ecaf6218484184e84ace62ea941f003a5b39e
Hana Miklos <hana.e.jakobsson@gmail.com>
2022-03-29 12:08:57 +0200

    added structure for level + levelmanager

---
+++ Test_Loopguy/Level.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test_Loopguy
+{
+    public class Level
+    {
+        int id;
+        Rectangle cameraBounds;
+        //array of tiles
+        //list of game objects
+        //list of enemies
+        //list of corresponding entrances from different ids and their position
+
+        public void Update()
+        {
+            //update game objects
+            //update enemy ai
+
+            //do any animations
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //draw tiles and objects and enemies, in the correct order
+        }
+    }
+    public class Entrance
+    {
+        Vector2 position;
+        int idFrom;
+        int idTo;
+    }
+}
---
+++ Test_Loopguy/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test_Loopguy
+{
+    static public class LevelManager
+    {
+        private static Level currentLevel;
+
+        //have list of levels?
+
+
+        public static Level LoadLevel(int id)
+        {
+
+            //obviously shouldn't return null when done
+            return null;
+        }
+
+
+        /*
+        private static Tile[] TileLoad(int id)
+        {
+        
+        }
+        */
+
+        /*
+        private static List<LevelObjects> ObjectLoad(int id)
+        {
+            
+        }
+        */
+
+        /*
+        private static List<Enemy> EnemyLoad(int id)
+        {
+            
+        }
+        */
+    }
+}
