47ec0998b0ff388a7e85ada8a5ac9c3c2f592821
JHC1133 <joarhyltencavallius@gmail.com>
2022-04-06 16:50:34 +0200

    StateManager

---
+++ Test_Loopguy/StateManager.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Loopguy
+{
+    class StateManager
+    {
+
+        public enum GameState
+        {
+            Menu,
+            InGame,
+            InEditor,
+            Load,
+        }
+
+        public static GameState currentState = GameState.Menu;
+
+        public static void Update(GameTime gameTime)
+        {
+            switch (currentState)
+            {
+                case GameState.Menu:
+
+                    MenuManager.Update(gameTime);
+
+                    break;
+                case GameState.InGame:
+                    break;
+                case GameState.InEditor:
+                    break;
+                case GameState.Load:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            switch (currentState)
+            {
+                case GameState.Menu:
+
+                    MenuManager.Draw(spriteBatch);
+
+                    break;
+                case GameState.InGame:
+                    break;
+                case GameState.InEditor:
+                    break;
+                case GameState.Load:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
