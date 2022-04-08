12f0a50453119c15b17423a3ca6115ba747e10dc
fizbook1 <77051952+fizbook1@users.noreply.github.com>
2022-04-08 15:05:16 +0200

    door foundations

--- Test_Loopguy/LevelManager.cs
+++ Test_Loopguy/LevelManager.cs
@@ -216,6 +216,7 @@ namespace Test_Loopguy
                 Vector2 objectPosition = new Vector2(0, 0);
                 objectPosition.X = Int32.Parse(splitter[1]);
                 objectPosition.Y = Int32.Parse(splitter[2]);
+                //int requiredKey = Int32.Parse(splitter[3]);
                 levelObjects.Add(ObjectCreator(objectToFind, objectPosition));
             }
 
