e949f27162717dc745418a4b26a07ac46e1ca327
Hana Miklos <hana.e.jakobsson@gmail.com>
2022-04-14 18:54:36 +0200

    keys

--- Test_Loopguy/Door.cs
+++ Test_Loopguy/Door.cs
@@ -10,7 +10,6 @@ namespace Test_Loopguy
     {
         int requiredKey;
         bool open = false;
-        Rectangle hitBox;
         Rectangle unlockArea;
         AnimSprite animation;
         public Door(Vector2 position, int requiredKey) : base(position)
@@ -20,7 +19,7 @@ namespace Test_Loopguy
             this.position = position;
             hitBox = new Rectangle((int)position.X, (int)position.Y, 32, 32);
             this.requiredKey = requiredKey;
-            unlockArea = new Rectangle((int)(position.X - 32), (int)(position.Y - 32), 128, 96);
+            unlockArea = new Rectangle((int)(position.X - 16), (int)(position.Y - 16), 64, 64);
             animation = new AnimSprite(TexMGR.door, new Point(32, 32));
         }
 
---
+++ Test_Loopguy/Pickup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test_Loopguy
+{
+    public class Pickup : LevelObject
+    {
+        public bool pickedUp = false;
+        public Pickup(Vector2 position) : base(position)
+        {
+            this.position = position;
+            this.hitBox = new Rectangle((int)position.X, (int)position.Y, 16, 16);
+        }
+
+        public void PickUp()
+        {
+            if(hitBox.Contains(EntityManager.player.centerPosition))
+            {
+                position.X = -10000;
+                position.Y = -10000;
+                pickedUp = true;
+                Effect();
+            }
+        }
+
+        protected virtual void Effect()
+        {
+
+        }
+    }
+
+    public class KeyPickup : Pickup
+    {
+        int id;
+        bool permanent;
+        public KeyPickup(Vector2 position, int id, bool permanent) : base(position)
+        {
+            this.position = position;
+            this.id = id;
+            this.permanent = permanent;
+            this.hitBox = new Rectangle((int)position.X, (int)position.Y, 16, 16);
+        }
+        protected override void Effect()
+        {
+            GiveKey();
+        }
+
+        private void GiveKey()
+        {
+            EntityManager.player.keys.Add(id);
+            if(permanent)
+            {
+                SaveKeyToFile();
+            }
+        }
+
+        private void SaveKeyToFile()
+        {
+            //add to save it to the savefile
+        }
+    }
+}
