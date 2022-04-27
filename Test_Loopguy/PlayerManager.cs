78f5968be648acc738908a1f475a68d18501eae4
JHC1133 <joarhyltencavallius@gmail.com>
2022-04-27 14:44:15 +0200

    Ändrade EntityManager till PlayerManager

---
+++ Test_Loopguy/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Loopguy
+{
+    class PlayerManager
+    {
+
+        public static Player player;
+        static List<Enemy> enemyList;
+
+        public static void PlayerInitialization()
+        {
+
+            player = new Player(new Vector2(64, 64));
+
+        }
+
+
+
+        public static void Update(GameTime gameTime)
+        {
+            player.Update(gameTime);
+
+ 
+
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            player.Draw(spriteBatch);
+
+        }
+
+    }
+}
