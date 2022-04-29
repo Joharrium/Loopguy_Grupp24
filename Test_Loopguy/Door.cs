8e89ee9e48a24430847ec6d44cab0da7b053543b
fizbook1 <77051952+fizbook1@users.noreply.github.com>
2022-04-29 21:09:27 +0200

    forgor door hitbox

--- Test_Loopguy/Door.cs
+++ Test_Loopguy/Door.cs
@@ -25,6 +25,7 @@ namespace Test_Loopguy
             sourceRectangle = new Rectangle(0, 0, 32, 32);
             this.position = position;
             hitBox = new Rectangle((int)position.X, (int)position.Y, 32, 32);
+            footprint = hitBox;
             this.requiredKey = requiredKey;
             unlockArea = new Rectangle((int)(position.X - 16), (int)(position.Y - 16), 64, 64);
             animation = new AnimatedSprite(TextureManager.door, new Point(32, 32));
@@ -78,6 +79,7 @@ namespace Test_Loopguy
                 hitBox.Y = 0;
                 hitBox.Width = 0;
                 hitBox.Height = 0;
+                footprint = hitBox;
                 open = true;
                 animation.Position = position;
                 
