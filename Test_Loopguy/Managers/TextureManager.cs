512f0374eccf23e8553ec3fb91a41e7436c6ddb0
JHC1133 <joarhyltencavallius@gmail.com>
2022-05-16 18:40:41 +0200

    Uppdaterade EnemyRangedSpriteSheet

--- Test_Loopguy/Managers/TextureManager.cs
+++ Test_Loopguy/Managers/TextureManager.cs
@@ -162,7 +162,7 @@ namespace Test_Loopguy
         {
             enemyPlaceholder = c.Load<Texture2D>("gfx/characters/enemy_placeholder");
             playerCharacterForMenu = c.Load<Texture2D>("LOOP_FELLA");
-            robotEnemySheet = c.Load<Texture2D>("rangedRobotEnemySpriteSheet");
+            robotEnemySheet = c.Load<Texture2D>("updatedRangedRobotEnemySpriteSheet");
         }
         
         private static void LoadParticles(ContentManager c)
