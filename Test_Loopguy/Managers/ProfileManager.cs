03c466c1f5c08086340ab3a3527fd18fe1307bc0
fizbook1 <77051952+fizbook1@users.noreply.github.com>
2022-05-17 08:46:02 +0200

    can change profiles now

--- Test_Loopguy/Managers/ProfileManager.cs
+++ Test_Loopguy/Managers/ProfileManager.cs
@@ -6,11 +6,16 @@ namespace Test_Loopguy
 {
     public static class ProfileManager
     {
+        static List<Profile> profiles = new List<Profile>();
         static Profile currentProfile;
 
         public static void Init()
         {
-            currentProfile = new Profile(1);
+            profiles.Add(new Profile(1));
+            profiles.Add(new Profile(2));
+            profiles.Add(new Profile(3));
+            currentProfile = profiles[0];
+            
         }
 
         public static void AddKey(int key)
@@ -21,6 +26,18 @@ namespace Test_Loopguy
         public static List<int> GetKeys()
         {
             return currentProfile.GetKeys();
+        }
+
+        public static void ChangeProfile()
+        {
+            currentProfile = profiles[(CurrentProfileId() + 1) % profiles.Count];
+            LevelManager.Reset();
+            
+        }
+
+        public static int CurrentProfileId()
+        {
+            return currentProfile.Id;
         }
     }
 }
