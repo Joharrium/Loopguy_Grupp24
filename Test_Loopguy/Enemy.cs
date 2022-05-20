7cdade669a322a97d0ab3118243c725e180c5872
Edward <ak0420@mau.se>
2022-05-20 14:00:02 +0200

    we gonna also start making maps in master

--- Test_Loopguy/Enemy.cs
+++ Test_Loopguy/Enemy.cs
@@ -635,7 +635,7 @@ namespace Test_Loopguy
             yOffset = texture.Height;
             this.maxHealth = 1;
             this.maxSpeed = 95;
-            aggroRange = 175;
+            aggroRange = 110;
             damage = 1;
             knockBackDistance = 160;
             knockBackDuration = 160;
@@ -662,6 +662,8 @@ namespace Test_Loopguy
 
             direction = distBetweenPlrAndEnemy;
             speed = maxSpeed;
+
+            Debug.WriteLine(distBetweenPlrAndEnemy.ToString());
         }
 
         public override void Movement(float deltaTime)
